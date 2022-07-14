906d948a5188f937148285fdd41092a9f0449e0f
Rathan <rathan.muralidhar@gmail.com>
2022-07-14 07:34:20 +0530

    Some Changes

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
 def fun(a,b):
     return a+b
-    
+
 """
 git init - initializes a new repository
 git status - provides cur. status (Which files are added / not)
@@ -13,4 +13,5 @@ git branch branch_name - creates a new branch
 git checkout branch_name - switches to a different branch
 git branch - shows all branches and current branch in green
 git commit -a - Add all files and commit simultaneously
+git branch -d branch_name - deletes the branch
 """

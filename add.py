0818405aded70c57b2e7a52323f10ce5aa74cfa6
Rathan <rathan.muralidhar@gmail.com>
2022-07-14 07:27:34 +0530

    Added Git Documentation

--- add.py
+++ add.py
@@ -1,2 +1,15 @@
 def fun(a,b):
     return a+b
+"""
+git init - initializes a new repository
+git status - provides cur. status (Which files are added / not)
+git log - Shows log of previous commits
+git add -A
+or
+git add --all - adds all the untracked changes to current commit
+git commit -m "Message" - commits to a version with "Message"
+git branch branch_name - creates a new branch
+git checkout branch_name - switches to a different branch
+git branch - shows all branches and current branch in green
+git commit -a - Add all files and commit simultaneously
+"""

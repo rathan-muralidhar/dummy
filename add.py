e407cdfba5f2eff3f945927fe3a8ce203bed11b0
Rathan <rathan.muralidhar@gmail.com>
2022-07-14 07:35:53 +0530

    fun changed to add

--- add.py
+++ add.py
@@ -1,15 +1,15 @@
-def fun(a,b):
-    return a+b
+def add(a,b):
+    c = a+b
+    return c
 """
 git init - initializes a new repository
 git status - provides cur. status (Which files are added / not)
 git log - Shows log of previous commits
-git add -A
-or
-git add --all - adds all the untracked changes to current commit
+git add -A or git add --all - adds all the untracked changes to current commit
 git commit -m "Message" - commits to a version with "Message"
 git branch branch_name - creates a new branch
 git checkout branch_name - switches to a different branch
 git branch - shows all branches and current branch in green
 git commit -a - Add all files and commit simultaneously
+git branch -d branch_name - deletes the branch
 """
